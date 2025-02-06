42cf6337674fb43046b3a8bffbf111a3178ed620
lord97j <lord97j@gmail.com>
2025-02-06 16:14:20 +0000

    Add sync-issues script, update funding, and categorize backend rule
    This commit introduces several minor updates:
    - Add a new npm script for syncing issues
    - Update Ko-fi username in FUNDING.yml
    - Add category to backend rule in JSON configuration

---
+++ scripts/github/job.js
@@ -0,0 +1,56 @@
+/* eslint-disable */
+const cron = require('node-cron');
+const syncPost = require('./syncPost');
+const syncCategories = require('./syncCategories');
+const syncRules = require('./syncRules');
+
+// https://www.npmjs.com/package/node-cron
+// # ┌────────────── second (optional)
+// # │ ┌──────────── minute
+// # │ │ ┌────────── hour
+// # │ │ │ ┌──────── day of month
+// # │ │ │ │ ┌────── month
+// # │ │ │ │ │ ┌──── day of week
+// # │ │ │ │ │ │
+// # │ │ │ │ │ │
+// # * * * * * *
+// const timeString = '15 * * * *'
+const timeString = '* 0 1 * * *'; // Running a job at 01:00 every day
+
+let job = null;
+
+function stopJob() {
+	if (job) {
+		job.stop();
+		job = null;
+	}
+}
+
+function startJob() {
+	stopJob();
+	job = cron.schedule(
+		timeString,
+		() => {
+			// eslint-disable-next-line no-console
+			console.log('🚀🚀 同步issue到blog文件');
+			syncPost();
+			console.log('🚀🚀 同步categories');
+			syncCategories();
+			console.log('🚀🚀 同步rules');
+			syncRules();
+		},
+		{
+			scheduled: true,
+			timezone: 'Asia/Shanghai'
+		}
+	);
+
+	job.start();
+	console.log('====================================');
+	console.log('🚀🚀 同步代码定时任务已开启');
+	console.log('====================================');
+}
+
+// syncPost() // 先执行一次
+
+export { startJob };
---
+++ scripts/sync-issues.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const syncPost = require('./github/syncPost');
+const syncCategories = require('./github/syncCategories');
+const syncRules = require('./github/syncRules');
+
+syncPost();
+syncCategories();
+syncRules();
